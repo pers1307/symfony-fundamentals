eb8b7633d1558502513be626fbb070671e007c20
pers1307 <you@example.com>
2024-02-06 15:42:44 +0500

    Добавил статику в проект

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -13,27 +13,31 @@ class VinylController extends AbstractController
     #[Route('/')]
     public function homepage(): Response
     {
-//        dd(['qwe' => 123]);
+        //        dd(['qwe' => 123]);
         dump(['qwe' => 123]);
+
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        ];
 
         return $this->render('vinyl/homepage.html.twig', [
             'title' => 'Hello yellow',
-            'tracks' => [
-                'qwerty' => 'Hello',
-                'qwerty2' => 'yellow',
-            ],
+            'tracks' => $tracks,
         ]);
     }
 
     #[Route('/browse/{slug}')]
     public function browse(string $slug = null): Response
     {
-        $title = 'All';
+        $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
 
-        if ($slug) {
-            $title = u(str_replace('-', ' ', $slug))->title(true);
-        }
-
-        return new Response("It's browse: $title");
+        return $this->render('vinyl/browse.html.twig', [
+            'genre' => $genre,
+        ]);
     }
 }
